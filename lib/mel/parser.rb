fb51d64c486c24106bf9be40a940f0a50e44d669
emsk <emsk1987@gmail.com>
2017-09-11 21:26:14 +0900

    Fix `Lint/UnusedMethodArgument`

--- lib/mel/parser.rb
+++ lib/mel/parser.rb
@@ -2,13 +2,13 @@ require 'ripper'
 
 module Mel
   class Parser < Ripper
-    def on_def(method, args, body)
+    def on_def(method, args, _body)
       output = "def #{method}"
       output += "(#{args.join(', ')})" unless args.nil?
       puts output
     end
 
-    def on_defs(target, separator, method, args, body)
+    def on_defs(target, separator, method, args, _body)
       output = "#{target}#{separator}#{method}"
       output += "(#{args.join(', ')})" unless args.nil?
       puts output
